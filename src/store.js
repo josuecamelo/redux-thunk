d67dbf119a624949256ebe8d604fc3b86bf1235d
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2019-03-28 21:40:42 -0300

    Configurando Thunk

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers';
-
+import thunk from 'redux-thunk'
 export default () =>{
-    const enchanters = applyMiddleware();
+    const enchanters = applyMiddleware(thunk);
     return createStore(
         rootReducer,
         enchanters
