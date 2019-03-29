8d967fa811ec64b85a6159496b255270f662b0a7
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2019-03-28 22:34:39 -0300

    Async Action

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+//import from './actions';
 
 class App extends Component {
+  componentDidMount() {
+    console.log('Mounted');
+
+    //this.props.fetchData();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    data: state.data
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    //fetchData: () => dispatch(fn)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/actions/app.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+//ACTIONS TYPES
+export const IS_FETCHING = 'IS_FETCHING';
+export const FETCHED = 'FETCHED';
+export const FETCHED_ERROR = 'FETCHED_ERROR';
+
+//ACTIONS CREATORS
+export function isFetching(bool) {
+    return {
+        type: IS_FETCHING,
+        isLoading: bool
+    }
+}
+
+export function fetchedError(err) {
+    return {
+        type: FETCHED_ERROR,
+        error: err
+    }
+}
+
+export function fetched(data) {
+    return {
+        type: FETCHED,
+        data: data
+    }
+}
+
+//THUNKS
+export function thunkFetchData() {
+    return async (dispatch) => {
+        dispatch(isFetching(true));
+        try {
+            const {data} = await axios.get('http://api.icndb.com/jokes');
+            dispatch(fetched(data.value));
+            dispatch(isFetching(false));
+        } catch (err) {
+            dispatch(fetchedError(err));
+            dispatch(isFetching(false));
+        }
+    }
+}
